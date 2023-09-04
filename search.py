082f9742febf9a68357cd40a1e9c465fb5f0d55b
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2023-09-05 00:20:18 +0530

    Update search.py

--- search.py
+++ search.py
@@ -18,20 +18,40 @@ def find_files_in_bucket(bucket_name, file_name):
 
     return matching_files
 
+
+def download_file_from_s3(bucket_name, file_key, local_path):
+    try:
+        # Initialize the S3 client
+        s3 = boto3.client('s3')
+        # Download the file from S3 to the local system
+        s3.download_file(bucket_name, file_key, local_path)
+        return True
+    except Exception as e:
+        print("Some Error Ocuured while downloading the file \n")
+        print(f"Error: {str(e)}")
+        return False
+
+
 if __name__ == "__main__":
 
+    # setting the aws credentials in order to authorize the program to aws.
     session = Session()
     credentials = session.get_credentials()
     current_credentials = credentials.get_frozen_credentials()
 
     bucket_name = 'testsiliconbucket'
-    file_name = 'Algoanalytics.docx'
+    file_name = 'testing.jpg'
+    
+    local_path = r'C:\Users\hp\Downloads' + "\\"+ file_name
+
+    print(local_path)
 
     result = find_files_in_bucket(bucket_name, file_name)
 
     if result:
         print(f"Matching files in '{bucket_name}':")
         for file in result:
-            print(file)
+            print(f"The File path is:- s3://{bucket_name}/{file}")
+            download_file_from_s3(bucket_name, file, local_path)
     else:
         print(f"No matching files found in '{bucket_name}' for '{file_name}'.")

36adf4d4772d8fb11dc936d97064b7b93163a6e2
Sibasish Satapathy <78403992+sibasish934@users.noreply.github.com>
2024-01-07 10:57:42 +0530

    Update send_notifications_kubernetes_pods_error.sh

--- send_notifications_kubernetes_pods_error.sh
+++ send_notifications_kubernetes_pods_error.sh
@@ -24,6 +24,13 @@ jq '.items[] | select(.status.containerStatuses[].state.waiting.reason == "Image
   #curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" --data-urlencode "To=+$RECIPIENT_PHONE_NUMBER" --data-urlencode "From=+$TWILIO_PHONE_NUMBER" --data-urlencode "Body=$body" -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
   echo "Sending alert to $email and $RECIPIENT_PHONE_NUMBER"
 done
+# check for the OOMKilled errors
+jq '.items[] | select(.status.containerStatuses[].state.waiting.reason == "OOMKilled") | .metadata.name, .metadata.namespace, .status.containerStatuses[].state.waiting.reason' latest.json | while read pod_name pod_namespace pod_status pod_reason; do
+  body=$(echo "Pod: $pod_name in namespace $pod_namespace is in OOMkilled state with reason $pod_reason")
+  echo -e "Subject: $subject\n$body" | sendmail "$email"
+  #curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" --data-urlencode "To=+$RECIPIENT_PHONE_NUMBER" --data-urlencode "From=+$TWILIO_PHONE_NUMBER" --data-urlencode "Body=$body" -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
+  echo "Sending alert to $email and $RECIPIENT_PHONE_NUMBER"
+done
 
 # Check for other errors
 jq '.items[] | select(.status.phase == "Failed") | .metadata.name, .metadata.namespace, .status.phase' latest.json | while read pod_name pod_namespace pod_status pod_reason; do
@@ -31,4 +38,9 @@ jq '.items[] | select(.status.phase == "Failed") | .metadata.name, .metadata.nam
   echo -e "Subject: $subject\n$body" | sendmail "$email"
   #curl -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" --data-urlencode "To=+$RECIPIENT_PHONE_NUMBER" --data-urlencode "From=+$TWILIO_PHONE_NUMBER" --data-urlencode "Body=$body" -u "$TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN"
   echo "Sending alert to $email and $RECIPIENT_PHONE_NUMBER"
+done
+
+#if the pods are in running state then
+jq '.items[] | select(.status.phase == "Running")' latest.json | while read pod_name pod_namespace pod_status; do
+  echo "Pod: $pod_name in namespace $pod_namespace is running"
 done
